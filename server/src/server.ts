dcb1a0682f139097ead7621168b9f4d5b4bd49d3
MARCELOMAZZONETTO <MARCMAKER@OUTLOOK.COM>
2023-06-04 20:16:17 -0300

    telainicialestruturaweb

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get(`/users`, async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log(`🤢 HTTP Server running on http://localhost:3333`)
+  })
